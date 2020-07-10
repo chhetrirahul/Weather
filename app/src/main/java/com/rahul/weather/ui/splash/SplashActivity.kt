99e37acbfaa34bd357ce1a1c332b0e69ce2fbaa6
rahul_chhetri <immarshallchhetri@gmail.com>
2020-07-10 16:27:47 +1000

    Add recyclerview and card view dependency

--- app/src/main/java/com/rahul/weather/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/rahul/weather/ui/splash/SplashActivity.kt
@@ -34,6 +34,7 @@ class SplashActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
+        finish()
     }
 
     companion object {
